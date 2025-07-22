841cfc5f4f2a3c94e0e3bdcee9db158264003bbe
joselawis <joselawis@gmail.com>
2025-07-22 11:49:28 +0200

    balance test

--- src/test/java/com/lawis/junitapp/ejemplo/models/AccountTest.java
+++ src/test/java/com/lawis/junitapp/ejemplo/models/AccountTest.java
@@ -1,6 +1,7 @@
 package com.lawis.junitapp.ejemplo.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
@@ -10,7 +11,7 @@ import org.junit.jupiter.api.Test;
 class AccountTest {
 
     @Test
-    void testNombreCuenta() {
+    void testPersonAccount() {
         Account account = new Account("John Doe", new BigDecimal("1000.12346"));
 
         String expected = "John Doe";
@@ -18,6 +19,14 @@ class AccountTest {
 
         assertEquals(expected, actual);
         assertTrue(() -> actual.equals(expected));
+    }
+
+    @Test
+    void testBalanceAccount() {
+        Account account = new Account("Jane Doe", new BigDecimal("2000.54321"));
+
+        assertEquals(2000.54321, account.getBalance().doubleValue());
+        assertFalse(() -> account.getBalance().compareTo(BigDecimal.ZERO) < 0);
     }
 
 }
