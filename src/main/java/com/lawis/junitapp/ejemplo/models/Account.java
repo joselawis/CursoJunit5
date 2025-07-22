0a09e88f746d913c2cbea5089631f97633aeb23c
joselawis <joselawis@gmail.com>
2025-07-22 11:37:24 +0200

    assertequals and asserttrue

--- src/main/java/com/lawis/junitapp/ejemplo/models/Account.java
+++ src/main/java/com/lawis/junitapp/ejemplo/models/Account.java
@@ -3,12 +3,17 @@ package com.lawis.junitapp.ejemplo.models;
 import java.math.BigDecimal;
 
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Account {
+    @NonNull
     private String person;
+    @NonNull
     private BigDecimal balance;
 
 }
--- src/test/java/com/lawis/junitapp/ejemplo/models/AccountTest.java
+++ src/test/java/com/lawis/junitapp/ejemplo/models/AccountTest.java
@@ -1,5 +1,23 @@
 package com.lawis.junitapp.ejemplo.models;
 
-public class AccountTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+    @Test
+    void testNombreCuenta() {
+        Account account = new Account("John Doe", new BigDecimal("1000.12346"));
+
+        String expected = "John Doe";
+        String actual = account.getPerson();
+
+        assertEquals(expected, actual);
+        assertTrue(() -> actual.equals(expected));
+    }
 
 }
