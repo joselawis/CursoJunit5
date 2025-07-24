e40d0d7c8cb408e71491386e846449de67bb56fe
joselawis <joselawis@gmail.com>
2025-07-24 11:37:23 +0200

    added logic to debit and credit

--- src/main/java/com/lawis/junitapp/ejemplo/models/Account.java
+++ src/main/java/com/lawis/junitapp/ejemplo/models/Account.java
@@ -19,11 +19,11 @@ public class Account {
     private BigDecimal balance;
 
     public void debit(BigDecimal amount) {
-
+        this.balance = this.balance.subtract(amount);
     }
 
     public void credit(BigDecimal amount) {
-
+        this.balance = this.balance.add(amount);
     }
 
 }
