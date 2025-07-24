206c80ba27a84d58b3751b9d31601b1ef36bc0a2
joselawis <joselawis@gmail.com>
2025-07-24 11:54:27 +0200

    class Bank with method transfer

---
+++ src/main/java/com/lawis/junitapp/ejemplo/models/Bank.java
@@ -0,0 +1,21 @@
+package com.lawis.junitapp.ejemplo.models;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Bank {
+
+    @NonNull
+    private String name;
+
+    public void transfer(Account from, Account to, BigDecimal amount) {
+    }
+
+}
